024db0dc5552ecf0e55dbf811150928fb58cbfd5
shakiv633 <shakibshuvo633@gmail.com>
2022-04-19 23:40:45 +0600

    deploy

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,21 +1,24 @@
 
 import React, { useRef } from "react";
-import { Form, Toast } from "react-bootstrap";
-import { useSendPasswordResetEmail,useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Button, Form, ToastContainer } from "react-bootstrap";
+import { useSendPasswordResetEmail,useSignInWithEmailAndPassword,useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
-import SocialLogin from "../SocialLogin/SocialLogin";
-
 import './Login.css'
 import Loading from "../../Shared/Loading/Loading";
+import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
     const emailRef=useRef('');
     const passwordRef=useRef('');
-    const navigate=useNavigate();
-    const location=useLocation();
-    let from=location.state?.from?.pathname||"/"
-    let errorElement;
+    const[sendPasswordResetEmail,sending]=useSendPasswordResetEmail(auth);
+    
+    const handleLogin=e=>{
+        e.preventDefault();
+        const email=emailRef.current.value;
+        const password=passwordRef.current.value;
+        signInWithEmailAndPassword(email,password);
+    }
 
 
     const[
@@ -24,47 +27,51 @@ const Login = () => {
         loading,
         error,
     ]=useSignInWithEmailAndPassword(auth);
-    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
-
-    if (loading || sending) {
-        return <Loading></Loading>
-    }
-    if(user){
+     const navigate=useNavigate();
+     const location=useLocation();
+     let from =location.state?.from?.pathname|| "/";
+     if(user){
         navigate(from, {replace: true});
-    }
+     }
+     let loginError;
     if (error) {
-        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+        loginError = <p className='text-danger'>{"Error: enter a valid mail"}</p>
     }
 
-
-    const submit=e=>{
-        e.preventDefault();
-        const email=emailRef.current.value
-        const password=passwordRef.current.value
-
-        signInWithEmailAndPassword(email,password);
-
-    }
-      const register=(e)=>{
-             navigate("/register")
-      }
-      const resetPassword = async () => {
-        const email = emailRef.current.value;
-        if (email) {
-            await sendPasswordResetEmail(email);
-            Toast('Sent email');
-        }
-        else{
-            Toast('please enter your email address');
-        }
+    const[signInWithGoogle, googleUser, googleLoading, googleError]=useSignInWithGoogle(auth);
+    let errorElement='';
+    if(googleError){
+        errorElement=<p>{'Error:closed by user'}</p>
     }
 
+    if(googleUser){
+        navigate(from, {replace: true});
+     }
+
+     if(googleLoading||loading){
+         return <Loading></Loading>
+     }
+
+     const resetPassword=async()=>{
+         const email=emailRef.current.value;
+         if(email){
+             await sendPasswordResetEmail(email);
+            return (' sent email')
+         }
+         else{
+            return ('entere your mail')
+         }
+     }
+
+     if (sending){
+         return <Loading></Loading>
+     }
   return (
    <div className="background">
        <p className="text-center mt-3 text-success mb-3 fw-bold fs-2">please login</p>
         <div className=" mt-3 container w-50 mx-auto">
      
-      <Form onSubmit={submit}>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="" controlId="formBasicEmail">
           <Form.Label></Form.Label>
           <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
@@ -77,15 +84,20 @@ const Login = () => {
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <button  className="border btn btn-primary shadow-md rounded p-1 fw-normal" variant="primary" type="submit">
+        {loginError}
+        <Button  className="border btn btn-primary shadow-md rounded p-1 fw-normal" variant="primary" type="submit">
           Sign In
-        </button>
-        <p className="fw-bold text-primary">  New User ?  <Link to="/register" className="text-danger text-decoration-none pe-auto fw-bold" onClick={register}>Sign Up !</Link></p>
-        <SocialLogin></SocialLogin>
+        </Button>
+        {errorElement}
+        <p> Have No Account ? <Link to="/register"> Resister</Link></p>
+      <p>Forget Your Password ? <Button variant='link' onClick={resetPassword}>Reset Password</Button> </p>
+      <SocialLogin></SocialLogin>
       </Form>
       
+
+      </div>
     </div>
-   </div>
+
     
   );
 };
